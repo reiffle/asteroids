537cf17f671ec49a58f488262dddeaafba927028
reiffle <pederreiff@gmail.com>
2025-01-16 12:10:48 -0600

    Starting to build asteroids

---
+++ main.py
@@ -0,0 +1,12 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    print ("Starting asteroids!")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen heighT: {SCREEN_HEIGHT}")
+
+main()
